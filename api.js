e7d3e8bd12b80c332788f1980bad9970e05a7acb
Andres <atm1996.ast@gmail.com>
2021-03-12 02:00:42 -0600

    segundo commit

--- api.js
+++ api.js
@@ -1,14 +1,12 @@
-let http = new XMLHttpRequest();
-http.open("GET", 'https://api.republicofdevs.com/api/v1/zipcodes', true);
-http.setRequestHeader('Content-type', 'text/html; charset=UTF-8');
-http.onreadystatechange = function() {
-    if (http.readyState == 4) {
-        if (http.status == 200){
-            OkCallback(JSON.parse(http.responseText));
-        }
-        else
-            ErrorCallback(http);
-    }
-};
-http.onerror = OkCallback;
-http.send(text);
+const url = 'https://api.republicofdevs.com/api/v1/zipcodes';
+var datos = [];
+
+let table = document.getElementById("data");
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        datos.push(...data.content);
+
+        console.log(datos);
+    })
+    .catch(err => console.log(err))
